6ca54f99daf1dac41c285efda532c99af826a4db
seungdols <seungdols0822@gmail.com>
2017-10-24 00:54:53 +0900

    테스트 주도 개발 - 4장. 프라이버시 실습 코드
    * 동치성 문제에 따른 테스트 코드 수정

--- src/test/java/com/seungdols/tdd/ex1/TestExample.java
+++ src/test/java/com/seungdols/tdd/ex1/TestExample.java
@@ -31,6 +31,13 @@ public class TestExample {
 	}
 
 	@Test
+	public void testMultiplication_4장_테스트코드() {
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+
+	@Test
 	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
